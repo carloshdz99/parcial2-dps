e7bab81a67bb8797e39cc4016248181c4a306f9e
landita <landainitpy@gmail.com>
2020-09-30 12:45:00 -0600

    configuraciones iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path:'', component:NavbarComponent, children:[
+          // componentes de compras, clientes, etc
+        ]
+      },
+      {path:'login', component:LoginComponent},
+      {path:'**', component:NotFoundComponent}
+    ]),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyCDmsOAIepwoOx2n0BDFHGg46iCcopJI9M",
+    authDomain: "db-dps-2.firebaseapp.com",
+    databaseURL: "https://db-dps-2.firebaseio.com",
+    projectId: "db-dps-2",
+    storageBucket: "db-dps-2.appspot.com",
+    messagingSenderId: "279082794948",
+    appId: "1:279082794948:web:53c55ba740e70590f4ae91",
+    measurementId: "G-RJPTRM51HQ"
+  }
 };
 
 /*
