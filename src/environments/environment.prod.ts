3f17780fc215b07917436bbb22f87b872db33575
carloshdz99 <carloshdzfunes@gmail.com>
2020-10-05 09:38:54 -0600

    host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyCDmsOAIepwoOx2n0BDFHGg46iCcopJI9M",
+    authDomain: "db-dps-2.firebaseapp.com",
+    databaseURL: "https://db-dps-2.firebaseio.com",
+    projectId: "db-dps-2",
+    storageBucket: "db-dps-2.appspot.com",
+    messagingSenderId: "279082794948",
+    appId: "1:279082794948:web:53c55ba740e70590f4ae91",
+    measurementId: "G-RJPTRM51HQ"
+  }
 };
